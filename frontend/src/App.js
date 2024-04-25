4ef3bf1539045417d6677b51e9b0f66908d038a0
Jun-Sang Kim <junsange@iastate.edu>
2024-04-25 18:08:38 -0500

    ..

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,4 @@
-// import logo from './logo.svg';
 import './App.css';
-
 import { useState, useEffect } from "react";
 
 function App() {
@@ -18,7 +16,6 @@ function App() {
 
   const [menu, setMenu] = useState(2);
 
-  // new Product
   const [addNewProduct, setAddNewProduct] = useState({
     _id: 0,
     title: "",
@@ -33,7 +30,6 @@ function App() {
 
   useEffect(() => {
     getAllProducts();
-    // eslint-disable-next-line
   }, [checked4]);
 
   function getAllProducts() {
@@ -49,7 +45,6 @@ function App() {
 
   function getOneProduct(id) {
     console.log(id);
-
     if (id >= 1) {
       fetch("http://localhost:4000/" + id)
         .then((response) => response.json())
@@ -112,14 +107,12 @@ function App() {
         console.log("Post a new product completed");
         console.log(data);
         if (data) {
-          //const keys = Object.keys(data);
           const value = Object.values(data);
           alert(value);
           window.location.reload();
         }
       });
   }
-
 
   function getOneByOneProductNext() {
     if (product.length > 0) {
@@ -169,7 +162,6 @@ function App() {
         console.log("Delete a product completed : ", deleteid);
         console.log(data);
         if (data) {
-          //const keys = Object.keys(data);
           const value = Object.values(data);
           alert(value);
         }
@@ -191,7 +183,6 @@ function App() {
         console.log("Updating the product's price completed : ", updateid);
         console.log(data);
         if (data) {
-          //const keys = Object.keys(data);
           const value = Object.values(data);
           alert(value);
         }
@@ -206,42 +197,44 @@ function App() {
       <div className='card border border-dark' style={{ width: `18rem` }}>
         <img src={el.image} width={20} alt={el.title} className='card-img-top' />
         <div className='card-body border border-dark' style={{ background: `lightgray` }}>
-          <p className='card-text'><span className='fw-bold'><u>Title:</u></span> {el.title}</p>
-          <p className='card-text'><span className='fw-bold'><u>Category:</u></span> {el.category}</p>
-          <p className='card-text'><span className='fw-bold'><u>Price:</u></span> {el.price}</p>
-          <p className='card-text'><span className='fw-bold'><u>Rate:</u></span> {el.rating.rate} <span className='fw-bold'><u>Count:</u></span> {el.rating.count}</p>
+          <p className='card-text'><span className='fw-bold'>Title:</span> {el.title}</p>
+          <p className='card-text'><span className='fw-bold'>Category:</span> {el.category}</p>
+          <p className='card-text'><span className='fw-bold'>Price:</span> {el.price}</p>
+          <p className='card-text'><span className='fw-bold'>Rate:</span> {el.rating.rate} <span className='fw-bold'>Count:</span> {el.rating.count}</p>
+          <p className='card-text'><span className='fw-bold'>Description:</span> {el.description}</p>
         </div>
       </div>
     </div>
   ));
 
   const showOneItem = oneProduct.map((el) => (
-    <div key={el._id}>
+    <div key={el._id} style={{marginLeft: '42%', marginTop: '1%'}}>
       <div className='card border border-dark' style={{ width: `18rem` }}>
         <img src={el.image} width={20} alt={el.title} className='card-img-top' />
         <div className='card-body border border-dark' style={{ background: `lightgray` }}>
-          <p className='card-text'><span className='fw-bold'><u>Title:</u></span> {el.title}</p>
-          <p className='card-text'><span className='fw-bold'><u>Category:</u></span> {el.category}</p>
-          <p className='card-text'><span className='fw-bold'><u>Price:</u></span> {el.price}</p>
-          <p className='card-text'><span className='fw-bold'><u>Rate:</u></span> {el.rating.rate} <span className='fw-bold'><u>Count:</u></span> {el.rating.count}</p>
+          <p className='card-text'><span className='fw-bold'>Title:</span> {el.title}</p>
+          <p className='card-text'><span className='fw-bold'>Category:</span> {el.category}</p>
+          <p className='card-text'><span className='fw-bold'>Price:</span> {el.price}</p>
+          <p className='card-text'><span className='fw-bold'>Rate:</span> {el.rating.rate} <span className='fw-bold'>Count:</span> {el.rating.count}</p>
+          <p className='card-text'><span className='fw-bold'>Description:</span> {el.description}</p>
         </div>
       </div>
     </div>
   ));
 
   return (
-    <div style={{ background: `linear-gradient(lightblue, lightgreen)`, minHeight: `100vh` }}>
+    <div style={{ minHeight: `100vh` }}>
       <div style={{ textAlign: 'center' }}>
         <h1>Assignment #3: Catalog of Products</h1>
-        <nav className="navbar navbar-expand-lg bg-body-tertiary border border-dark" style={{ background: `radial-gradient(#cfcfcf, #a2b0a3)` }}>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary border border-dark" style={{background: '#010203'}} >
           <div className="container-fluid">
             <div className="collapse navbar-collapse justify-content-center">
               <div className="btn-group-lg" role="group">
-                <button className="btn btn-success" style={{ marginLeft: `15px`, marginRight: `15px` }} onClick={() => setMenu(1)}>Create</button>
-                <button className="btn btn-success" aria-current="page" style={{ marginLeft: `15px`, marginRight: `15px` }} onClick={() => setMenu(2)}>Read</button>
-                <button className="btn btn-success" style={{ marginLeft: `15px`, marginRight: `15px` }} onClick={() => setMenu(3)}>Update</button>
-                <button className="btn btn-success" style={{ marginLeft: `15px`, marginRight: `15px` }} onClick={() => setMenu(4)}>Delete</button>
-                <button className="btn btn-success" style={{ marginLeft: `15px`, marginRight: `15px` }} onClick={() => setMenu(5)}>Credits</button>
+                <button className="btn btn-danger" style={{ marginLeft: `15px`, marginRight: `15px` }} onClick={() => setMenu(1)}>Create</button>
+                <button className="btn btn-danger" aria-current="page" style={{ marginLeft: `15px`, marginRight: `15px` }} onClick={() => setMenu(2)}>Read</button>
+                <button className="btn btn-danger" style={{ marginLeft: `15px`, marginRight: `15px` }} onClick={() => setMenu(3)}>Update</button>
+                <button className="btn btn-danger" style={{ marginLeft: `15px`, marginRight: `15px` }} onClick={() => setMenu(4)}>Delete</button>
+                <button className="btn btn-danger" style={{ marginLeft: `15px`, marginRight: `15px` }} onClick={() => setMenu(5)}>About Us</button>
               </div>
             </div>
           </div>
@@ -250,21 +243,20 @@ function App() {
 
       <div className='m-4'>
         {menu === 2 && <div>
-          <h1 className='text-center fs-1 fw-bold text-success fw-underline'><u>View Products:</u></h1>
-          <h1><u>Show All Available Products:</u></h1>
-          <button className='btn btn-success btn-lg' onClick={() => getAllProducts()}>Toggle Showing All Products</button>
+          <h1 className='text-center text-danger'>Show All Products</h1>
+          <div id='showall'><button className='btn btn-danger btn-lg' onClick={() => getAllProducts()}>Show All</button></div>
           <hr></hr>
-          {viewer1 && <div><span className='fs-2'>Products:</span><span className='row row-cols-auto'>{showAllItems}</span></div>}
+          {viewer1 && <div><span className='row row-cols-auto'>{showAllItems}</span></div>}
 
           <hr></hr>
-          <h1><u>Show One Product by ID:</u></h1>
+          <h1 className='text-center text-danger'>Show One Product by ID</h1>
           <input type="text" id="message" name="message" placeholder="id" className='form-control form-control-lg' style={{ maxWidth: `10vw` }} onChange={(e) => getOneProduct(e.target.value)} />
-          {viewer2 && <div><span className='fs-2'>Product:</span> {showOneItem}</div>}
+          {viewer2 && <div>{showOneItem}</div>}
           <hr></hr>
         </div>}
 
         {menu === 1 && <div>
-          <h1 className='text-center fs-1 fw-bold text-success fw-underline'><u>Add a New Product:</u></h1>
+          <h1 className='text-center fs-1 fw-bold text-danger fw-underline'>Add a New Product</h1>
           <form style={{ maxWidth: `50vw`, marginLeft: `25vw` }}>
             <div className="row mb-3">
               <label className="col-sm-2 col-form-label col-form-label-lg">Product ID</label>
@@ -275,41 +267,41 @@ function App() {
             <div className="row mb-3">
               <label className="col-sm-2 col-form-label col-form-label-lg">Name</label>
               <div className="col-sm-10">
-                <input type="text" placeholder="Title" className="form-control form-control-lg" name="title" value={addNewProduct.title} onChange={handleChange} />
+                <input type="text" className="form-control form-control-lg" name="title" value={addNewProduct.title} onChange={handleChange} />
               </div>
             </div>
             <div className="row mb-3">
               <label className="col-sm-2 col-form-label col-form-label-lg">Price</label>
               <div className="col-sm-10">
-                <input type="number" placeholder="Price" className="form-control form-control-lg" name="price" value={addNewProduct.price} onChange={handleChange} />
+                <input type="number" className="form-control form-control-lg" name="price" value={addNewProduct.price} onChange={handleChange} />
               </div>
             </div>
             <div className="row mb-3">
-              <label className="col-sm-2 col-form-label col-form-label-lg">Product Category</label>
+              <label className="col-sm-2 col-form-label col-form-label-lg">Category</label>
               <div className="col-sm-10">
-                <input type="text" placeholder="Category" className="form-control form-control-lg" name="category" value={addNewProduct.category} onChange={handleChange} />
+                <input type="text" className="form-control form-control-lg" name="category" value={addNewProduct.category} onChange={handleChange} />
               </div>
             </div>
             <div className="row mb-3">
-              <label className="col-sm-2 col-form-label col-form-label-lg">Image Filepath</label>
+              <label className="col-sm-2 col-form-label col-form-label-lg">Image URL</label>
               <div className="col-sm-10">
-                <input type="text" placeholder="Image" className="form-control form-control-lg" name="image" value={addNewProduct.image} onChange={handleChange} />
+                <input type="text" className="form-control form-control-lg" name="image" value={addNewProduct.image} onChange={handleChange} />
               </div>
             </div>
             <div className="row mb-3">
-              <label className="col-sm-2 col-form-label col-form-label-lg">Product Rating</label>
+              <label className="col-sm-2 col-form-label col-form-label-lg">Rating</label>
               <div className="col-sm-10">
-                <input type="number" placeholder="Rate" className="form-control form-control-lg" name="rate" value={addNewProduct.rating.rate} onChange={handleChange} />
+                <input type="number" className="form-control form-control-lg" name="rate" value={addNewProduct.rating.rate} onChange={handleChange} />
               </div>
             </div>
             <div className="row mb-3">
-              <label className="col-sm-2 col-form-label col-form-label-lg">Product Count</label>
+              <label className="col-sm-2 col-form-label col-form-label-lg">Count</label>
               <div className="col-sm-10">
-                <input type="number" placeholder="Count" className="form-control form-control-lg" name="count" value={addNewProduct.rating.count} onChange={handleChange} />
+                <input type="number" className="form-control form-control-lg" name="count" value={addNewProduct.rating.count} onChange={handleChange} />
               </div>
             </div>
             <div className="row mb-3">
-              <button type="submit" onClick={handleOnSubmit} className="btn btn-success col-auto">
+              <button type="submit" onClick={handleOnSubmit} className="btn btn-danger col-auto">
                 Submit
               </button>
             </div>
@@ -317,8 +309,7 @@ function App() {
         </div>}
 
         {menu === 4 && <div>
-          <h1 className='text-center fs-1 fw-bold text-success fw-underline'><u>Delete One Product:</u></h1>
-          
+          <h1 className='text-center fs-1 fw-bold text-danger fw-underline'>Delete One Product</h1>
           <div style={{width: `286px`, marginLeft: `41vw`}}>
             <div>
               <div className="row flex-nowrap">
@@ -329,18 +320,18 @@ function App() {
                 <button className='btn btn-secondary' onClick={() => getOneByOneProductNext()}>Next</button>
                 </div>
                 <div className="col">
-                <button className='btn btn-success' onClick={() => deleteOneProduct(product[index]._id)}>Delete</button>
+                <button className='btn btn-danger' onClick={() => deleteOneProduct(product[index]._id)}>Delete</button>
                 </div>
               </div>
             </div>
           <div key={product[index]._id}>
-            <div className='card border border-dark' style={{ width: `18rem` }}>
+            <div className='card border border-dark' style={{ width: `18rem`, marginTop: '4%' }}>
               <img src={product[index].image} width={20} alt={product[index].title} className='card-img-top' />
               <div className='card-body border border-dark' style={{ background: `lightgray` }}>
-                <p className='card-text'><span className='fw-bold'><u>Title:</u></span> {product[index].title}</p>
-                <p className='card-text'><span className='fw-bold'><u>Category:</u></span> {product[index].category}</p>
-                <p className='card-text'><span className='fw-bold'><u>Price:</u></span> {product[index].price}</p>
-                <p className='card-text'><span className='fw-bold'><u>Rate:</u></span> {product[index].rating.rate} <span className='fw-bold'><u>Count:</u></span> {product[index].rating.count}</p>
+                <p className='card-text'><span className='fw-bold'>Title:</span> {product[index].title}</p>
+                <p className='card-text'><span className='fw-bold'>Category:</span> {product[index].category}</p>
+                <p className='card-text'><span className='fw-bold'>Price:</span> {product[index].price}</p>
+                <p className='card-text'><span className='fw-bold'>Rate:</span> {product[index].rating.rate} <span className='fw-bold'>Count:</span> {product[index].rating.count}</p>
               </div>
             </div>
             </div>
@@ -348,7 +339,7 @@ function App() {
         </div>}
 
         {menu === 3 && <div>
-          <h1 className='text-center fs-1 fw-bold text-success fw-underline'><u>Update One Products's Price:</u></h1>
+          <h1 className='text-center fs-1 fw-bold text-danger fw-underline'>Update One Products's Price</h1>
           <div style={{width: `286px`, marginLeft: `41vw`}}>
             <div>
               <div className="row flex-nowrap">
@@ -361,15 +352,15 @@ function App() {
               </div>
             </div>
             <input style={{maxWidth: `50%`}} type="number" placeholder="New Price" name="updated_price" value={addNewPrice} onChange={handleUpdateChange} />
-            <button className='btn btn-success m-2' onClick={() => updateOneProduct(product[index2]._id, addNewPrice)}>Update Price</button>
+            <button className='btn btn-danger m-2' onClick={() => updateOneProduct(product[index2]._id, addNewPrice)}>Update Price</button>
             <div key={product[index2]._id}>
               <div className='card border border-dark' style={{ width: `18rem` }}>
                 <img src={product[index2].image} width={20} alt={product[index2].title} className='card-img-top' />
                 <div className='card-body border border-dark' style={{ background: `lightgray` }}>
-                  <p className='card-text'><span className='fw-bold'><u>Title:</u></span> {product[index2].title}</p>
-                  <p className='card-text'><span className='fw-bold'><u>Category:</u></span> {product[index2].category}</p>
-                  <p className='card-text'><span className='fw-bold'><u>Price:</u></span> {product[index2].price}</p>
-                  <p className='card-text'><span className='fw-bold'><u>Rate:</u></span> {product[index2].rating.rate} <span className='fw-bold'><u>Count:</u></span> {product[index2].rating.count}</p>
+                  <p className='card-text'><span className='fw-bold'>Title:</span> {product[index2].title}</p>
+                  <p className='card-text'><span className='fw-bold'>Category:</span> {product[index2].category}</p>
+                  <p className='card-text'><span className='fw-bold'>Price:</span> {product[index2].price}</p>
+                  <p className='card-text'><span className='fw-bold'>Rate:</span> {product[index2].rating.rate} <span className='fw-bold'>Count:</span> {product[index2].rating.count}</p>
                 </div>
               </div>
             </div>
@@ -377,7 +368,7 @@ function App() {
         </div>}
 
         {menu === 5 && <div>
-          <h1 className='text-center fs-1 fw-bold text-success fw-underline'><u>Credits</u></h1>
+          <h1 className='text-center fs-1 fw-bold text-danger fw-underline'>About Us</h1>
           <div className='text-center'>
             <p><span className='fw-bold'>Team:</span> #38</p>
             <p><span className='fw-bold'>Member #1:</span> Jun-Sang Kim (<a href="mailto:junsange@iastate.edu">junsange@iastate.edu</a>)</p>
